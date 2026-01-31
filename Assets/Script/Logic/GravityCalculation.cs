5395bcf0061bfb08e3d1537c2da9d87011e690f2
k0itoyuu <2246727686@qq.com>
2026-01-31 16:38:26 +0800

    计算重力之和传给rocketmove

---
+++ Assets/Script/Logic/GravityCalculation.cs
@@ -0,0 +1,24 @@
+/****************************************************
+    文件：GravityCalculation.cs
+	作者：k0itoyuu
+    日期：#CreateTime#
+	功能：Nothing
+*****************************************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityCalculation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
